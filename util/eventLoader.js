b8236305f217253ad7e2bb860373890de6239b29
Glitch (acciajsshbsdental-boiljejehed-marigolhhdeeb) <none>
2021-08-31 07:57:13 +0000

    Rewound to commit 0f7919e1fff6a4feb874e6ef4c5eec66df41d096 + 0 edits

--- util/eventLoader.js
+++
@@ -1,5 +0,0 @@
-const reqEvent = (event) => require(`../events/${event}`);
-module.exports = client => {
-  client.on('message', reqEvent('message'));
-};
-//== BEERCODE (https://discord.gg/ew3dpTu4Z5) BEERCODE ==\\
