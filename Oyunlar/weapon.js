33386e744f915ff4bc344850cd8853bdb42b3097
Glitch (acciajsshbsdental-boiljejehed-marigolhhdeeb) <none>
2021-08-31 07:58:37 +0000

    Rewound to commit ac8754ace794bf4a3250c02847b58a20e77ee6d8 + 0 edits

--- Oyunlar/weapon.js
+++ Oyunlar/weapon.js
@@ -2,20 +2,24 @@ const Discord = require('discord.js');
 var ayarlar = require("../ayarlar.json")
 const db = require('quick.db')
 exports.run = function(client, message,  args) {
-//== BEERCODE (https://discord.gg/ew3dpTu4Z5) BEERCODE ==\\
+//////////
   let miktar = args[0]
   let para = db.fetch(`para_${message.author.id}`)
   if(!miktar) return message.channel.send(new Discord.MessageEmbed()
       .setAuthor(message.author.tag, message.author.avatarURL({dynamic: true}))
       .setColor("GREEN")
-      .setDescription(`**slots** You Can't More Than<:emoji_61:867523675304624148> 50,000`))
+      .setDescription(`**WEAPON** You Can't More Than💰 50,000`))
   //== BEERCODE (https://discord.gg/ew3dpTu4Z5) BEERCODE ==\\
+if(miktar  < 50000) return message.channel.send(new Discord.MessageEmbed()
+      .setAuthor(message.author.tag, message.author.avatarURL({dynamic: true}))
+      .setColor("GREEN")
+      .setDescription(`**WEAPON**You Can't More Than💰 50,000`))
+  /////////////
+//== BEERCODE (https://discord.gg/ew3dpTu4Z5) BEERCODE ==\\
 if(miktar > 50000) return message.channel.send(new Discord.MessageEmbed()
       .setAuthor(message.author.tag, message.author.avatarURL({dynamic: true}))
       .setColor("GREEN")
-      .setDescription(`**slots**You Can't More Than💰 50,000`))
-  ////////
-    
+      .setDescription(`**WEAPON**You Can't More Than💰 50,000`))
   /////////////
 if(miktar > para) return message.channel.send(new Discord.MessageEmbed()
   .setColor("#ffff00")
@@ -26,17 +30,20 @@ if(miktar > para) return message.channel.send(new Discord.MessageEmbed()
 .setColor("RED")
 .setAuthor(message.author.tag, message.author.avatarURL({dynamic: true}))
 .setDescription(`🤔 The amount you entered is not a valid number !?`))
- let kazandin = miktar*3   
-  let slots = ["🍇","🍉","🍎","🍒"];
-//== BEERCODE (h"ttps://discord.gg/ew3dpTu4Z5) BEERCODE ==\\
+ let kazandin = miktar*20
+  let slots = ["<:emoji_53:872431707301761036>","<:emoji_54:872431734073991188>","<:emoji_51:872431638968148010>","<:emoji_52:872431670945529936>","<:emoji_48:872431561360957461>","<:emoji_49:872431584052121630>","<:emoji_50:872431618499969104>"];
+  //
+//== BEERCODE (https://discord.gg/ew3dpTu4Z5) BEERCODE ==\\
   let result1 = Math.floor((Math.random() * slots.length));
   let result2 = Math.floor((Math.random() * slots.length));
   let result3 = Math.floor((Math.random() * slots.length));
+  
 
   //
   let ust1 = Math.floor((Math.random() * slots.length));
   let ust2 = Math.floor((Math.random() * slots.length));
   let ust3 = Math.floor((Math.random() * slots.length));
+  
 
   //
   let alt1 = Math.floor((Math.random() * slots.length));
@@ -44,39 +51,32 @@ if(miktar > para) return message.channel.send(new Discord.MessageEmbed()
   let alt3 = Math.floor((Math.random() * slots.length));
   
   
+  
 //===BEERCODE (https://discord.gg/ew3dpTu4Z5) BEERCODE ==\\ 
-  if (slots[result1] === slots[result2] && slots[result2] === slots[result3] ) {
-    var text2 = `**
-\`\`\`__SLOTS__\`\`\`
-<a:emoji_55:869537420327157790><a:emoji_55:869537420327157790><a:emoji_55:869537420327157790> ${message.author.username}
-\`\`|       |\`\`
-**`;
+  if (slots[result1] === slots[result2] && slots[result2] === slots[result3]) {
+    var text2 = `<@${message.author.id}>Weapon⏰ Choice Own Or Lost`;
           message.channel.send(text2).then(msg => {
             setTimeout(() => {
               msg.edit(`**
-\`\`\`__SLOTS__\`\`\`
-${slots[result1]}${slots[result2]}${slots[result3]} ${message.author.username} bet💶 ${kazandin}
-\`\`|        |\`\` 
-was added to your balance  💶${kazandin}+ x3\`\`\`You win...\`\`\`  🟢 **`,true);
-            }, 3500);
+    ___WEAPON___  
+|${slots[result1]}${slots[result2]}${slots[result3]}|${message.author.username}💵 ${kazandin}+
+|                   |
+|                   |**`,true);
+            }, 3000);
           });
 
      db.add(`para_${message.author.id}`, kazandin)
 //== BEERCODE (https://discord.gg/ew3dpTu4Z5) BEERCODE ==\\
   } else {
-    var text3 = `**
-\`\`\`__SLOTS__\`\`\`
-<a:emoji_55:869537420327157790><a:emoji_55:869537420327157790><a:emoji_55:869537420327157790> ${message.author.username}
-\`\`|        |\`\`
-**`;
+    var text3 = `<@${message.author.id}>Weapon⏰ Choice Own Or Lost`;
           message.channel.send(text3).then(msg => {
             setTimeout(() => {
               msg.edit(`**
-\`\`\`__SLOTS__\`\`\`
-${slots[result1]}${slots[result2]}${slots[result3]} ${message.author.username} bet💶 ${miktar}-
-\`\`|        |\`\`
-and won nothing...\`\`\`You lost...\`\`\` 🔴  **`, true);
-            }, 3500);
+    ___WEAPON___  
+|${slots[result1]}${slots[result2]}${slots[result3]}|${message.author.username}💵 ${miktar}-
+|                   |
+|                   |**`, true);
+            }, 3000);
           });
   db.add(`para_${message.author.id}`, -miktar)
   }
@@ -85,10 +85,10 @@ and won nothing...\`\`\`You lost...\`\`\` 🔴  **`, true);
 exports.conf = {
   enabled: true,
   guildOnly: true,
-  aliases: ["slot","s","slotmachine"],
+  aliases: ["wn","we","w"],
   permLevel: 2
 };
 
 exports.help = {
-  name: 'slots',
-  };
+  name: 'weapon',
+};
