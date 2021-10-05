f2fa74efa38c67f046be7216de43c2d8361efea3
Glitch (acciajsshbsdental-boiljejehed-marigolhhdeeb) <none>
2021-10-05 16:50:16 +0000

    ☃️🏤 Checkpoint ./beercode.js:35495160/76

--- beercode.js
+++ beercode.js
@@ -100,7 +100,7 @@ c.react('').then(() =>
 });
 //===============================================================================\\
 client.on('guildCreate', guild => {
-client.channels.cache.get("874334714410975242")
+client.channels.cache.get("892069982077333577")
   .send(`
 ✅ **Join Server**: ${client.guilds.cache.size}
 🔠 **Server Name**: ${guild.name}
@@ -110,7 +110,7 @@ client.channels.cache.get("874334714410975242")
 });
 ////////////////////////////////////////////
 client.on('guildDelete', guild => {
-  client.channels.cache.get("874334850012807218")
+  client.channels.cache.get("892069952717193318")
   .send(`
 <@782911020527517727>
 ❎ **Lift Server**: ${client.guilds.cache.size}
