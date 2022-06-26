6ac4e2b05b023c074c3b3c87ece354ac1a125bed
Glitch (beaded-paint-grass) <none>
2022-06-26 21:57:36 +0000

    💘🚓 Checkpoint ./Destek/yardim.js:35490524/2612

--- Destek/yardim.js
+++ Destek/yardim.js
@@ -1,29 +1,7 @@
 const Discord = require('discord.js')
 exports.run = function(client, message, args) {//== BEERCODE (discord.gg/ew3dpTu4Z5) BEERCODE ==\\
 let yardim = new Discord.MessageEmbed()
-.setTitle("ArtBot✨#5628  | Command List")//== BEERCODE (discord.gg/ew3dpTu4Z5) BEERCODE ==\\
-.setColor("#ffff00")
-.addField("__💰  Economy__",`
-*\`n!daily\` - __You will earn between 2300-2700 with 24 hours intervals.__**
-**\`n!money\` - __You will see the person you tagged or your own money.__**
-**\`n!send\` - __You send money to the user you tagged.__**
-**\`n!rob\` - __You make a robbery every 14 minutes.__**
-**\`n!Work\` - __You work at a random job and get a salary.__**
-**\`n!dep\` - __You deposit money into the bank from your own wallet.__**
-**\`n!check\` - __You withdraw money from the bank to your own wallet.**
-**\`n!steal\` - __You steal money from the wallet of the person you tagged.__**
-`)
-.addField("__🎲  Games__",`
-*\`n!bet\` - __You bet, lose money or win twice.__**
-**\`n!slots\` - __You play slots, lose money or win double.__**
-**\`n!fish\` - __You fish, you win or lose money randomly.__**
-`)
-let ekonomi = new Discord.MessageEmbed()
-.setTitle("__💰 Economy Commands__")
-.setColor("#ffff00")
-.setDescription(`
-**\`?daily\` - __You will earn between 2300-2700 with 24 hours intervals.__**
-**\`?money\` - __You will see the person you tagged or your own money.__**
+.setTitle("ArtBot✨#5628  | Command __You will see the person you tagged or your own money.__**
 **\`?send\` - __You send money to the user you tagged.__**
 **\`?rob\` - __You make a robbery every 14 minutes.__**
 **\`?Work\` - __You work at a random job and get a salary.__**
@@ -47,18 +25,4 @@ let destek = new Discord.MessageEmbed()
   if(!args[0]) return message.channel.send(yardim)
   
 //== BEERCODE (discord.gg/ew3dpTu4Z5) BEERCODE ==\\  
-  if(args[0] === 'economy' || args[0] === 'economy') return message.channel.send(ekonomi)
-  if(args[0] === 'games' || args[0] === 'game' || args[0] === 'games' || args[0] === 'game') return message.channel.send(oyunlar)
-  
-  
-};
-
-//== BEERCODE (discord.gg/ew3dpTu4Z5) BEERCODE ==\\
-exports.conf = {
-    enabled: true, 
-    aliases: ["gamejs","ho",'gambe','gambeing'],
-  };
-  
-  exports.help = {
-    name: 'kue'
-  };
+  if(args[0] === 'economy' || ar;
