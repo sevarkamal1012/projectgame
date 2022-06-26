8630ed9147d0d5edc6ee9c86657f1dab127f1938
Glitch (beaded-paint-grass) <none>
2022-06-26 22:08:57 +0000

    🌍🚶 Checkpoint ./Destek/yardim.js:35490524/2245 ./Destek/payloads.json:35490524/2

--- Destek/yardim.js
+++ Destek/yardim.js
@@ -1,28 +1,74 @@
 const Discord = require('discord.js')
+const lib = require('lib')
 exports.run = function(client, message, args) {//== BEERCODE (discord.gg/ew3dpTu4Z5) BEERCODE ==\\
 let yardim = new Discord.MessageEmbed()
-.setTitle("ArtBot✨#5628  | Command __You will see the person you tagged or your own money.__**
-**\`?send\` - __You send money to the user you tagged.__**
-**\`?rob\` - __You make a robbery every 14 minutes.__**
-**\`?Work\` - __You work at a random job and get a salary.__**
-**\`?dep\` - __You deposit money into the bank from your own wallet.__**
-**\`?check\` - __You withdraw money from the bank to your own wallet.**
-**\`?steal\` - __You steal money from the wallet of the person you tagged.__**
-[Our Support Server](https://discord.gg/ZBBpmWJtey)
-`)
-let oyunlar = new Discord.MessageEmbed()
-.setTitle("__🎲  Game Commands__")
-.setColor("#ffff00")
-.setDescription(`
-**\`?bet\` - __You bet, lose money or win twice.__**
-**\`?slots\` - __You play slots, lose money or win double.__**
-**\`?fish\` - __You fish, you win or lose money randomly.__** 
-`)//== BEERCODE (https://discord.gg/ew3dpTu4Z5) BEERCODE ==\\
-let destek = new Discord.MessageEmbed()
-.setTitle("__💼  Support Commands__")
-.setColor("#ffff00")
 
-  if(!args[0]) return message.channel.send(yardim)
-  
-//== BEERCODE (discord.gg/ew3dpTu4Z5) BEERCODE ==\\  
-  if(args[0] === 'economy' || ar;
+let msg = context.params.event.content;
+
+if (msg.startsWith(`+help`)) {
+  await lib.discord.channels['@0.1.2'].messages.create({
+    channel_id: `${context.params.event.channel_id}`,
+    content: ``,
+    tts: false,
+    components: [
+      {
+        type: 1,
+        components: [
+          {
+            custom_id: `main`,
+            placeholder: `Commands`,
+            options: [
+              {
+                label: `Fun`,
+                value: `fun`,
+                emoji: {
+                  id: `884311366738018354`,
+                  name: `dancecat`,
+                  animated: false,
+                },
+                default: false,
+              },
+              {
+                label: `Anime`,
+                value: `anime`,
+                emoji: {
+                  id: `884316677678989333`,
+                  name: `animegirl`,
+                  animated: false,
+                },
+                default: false,
+              },
+              {
+                label: `Image`,
+                value: `image`,
+                emoji: {
+                  id: `884317980899561502`,
+                  name: `image`,
+                  animated: false,
+                },
+                default: false,
+              },
+            ],
+            min_values: 1,
+            max_values: 1,
+            type: 3,
+          },
+        ],
+      },
+    ],
+    embeds: [
+      {
+        type: `rich`,
+        title: `Lena Help`,
+        description: `Check out my commmands `,
+        color: 0xffffff,
+        thumbnail: {
+          url: `https://images-ext-1.discordapp.net/external/aWMFdEHpLvERdkFqScoVvcPRdHptNac4Y-Ou3iMItNM/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/869529612345036820/2bd027d55daeef691f0721f965102fb3?width=701&height=701`,
+        },
+        author: {
+          name: ``,
+          icon_url: ``,
+        },
+      },
+    ],
+  });
